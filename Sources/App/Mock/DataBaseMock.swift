58a1f6aa45ae18732766625d537e12f04b36581b
Ke4a <Helpomos@gmail.com>
2022-11-15 15:24:41 +0300

    add lore description

--- Sources/App/Mock/DataBaseMock.swift
+++ Sources/App/Mock/DataBaseMock.swift
@@ -26,7 +26,7 @@ final class DataBaseMock {
                                  category: index % 2 == 0 ? 1 : 2,
                                  name: "Товар \(index)",
                                  price: Int.random(in: 0...100_000),
-                                 description: "Мощный товар \(index)",
+                                 description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
                                  reviews: reviews))
         }
         return array
