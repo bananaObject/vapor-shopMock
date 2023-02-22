e703115bbd0ed75d562c3484dcf6ab60e74a0268
Ke4a <Helpomos@gmail.com>
2023-02-22 19:20:41 +0500

    fix logic

--- Sources/App/Mock/Product.swift
+++ Sources/App/Mock/Product.swift
@@ -16,9 +16,14 @@ struct Product {
     let description: String
     var reviews: [ReviewResponse]
     var images: [String] = ["https://i.ibb.co/gwx9KRB/06764f7f-62e8-4672-8f7a-6ebc7af537df.png",
-                            "https://i.ibb.co/wM2p7pG/076697a6-f720-4f94-b364-6853e9bd4083.png",
-                            "https://i.ibb.co/yn7sf2N/c8f2a301-f844-4b77-948c-b842e85c7709.png",
-                            "https://i.ibb.co/XZgCk1d/8c38e551-8cd0-41f8-9d3a-8d36b7a43243.png"]
+                            "https://i.ibb.co/b1wTNDR/076697a6-f720-4f94-b364-6853e9bd4083.png",
+                            "https://i.ibb.co/vw8gvfh/C8f2a301-f844-4b77-948c-b842e85c7709.png",
+                            "https://i.ibb.co/TbVSjQX/8c38e551-8cd0-41f8-9d3a-8d36b7a43243.png"]
+
+    var smallImages: [String] = ["https://i.ibb.co/Qfj4y9M/06764f7f-62e8-4672-8f7a-6ebc7af537df.png",
+                                 "https://i.ibb.co/nB7SZLQ/076697a6-f720-4f94-b364-6853e9bd4083.png",
+                                 "https://i.ibb.co/sbckT9p/C8f2a301-f844-4b77-948c-b842e85c7709.png",
+                                 "https://i.ibb.co/2jm4KMw/8c38e551-8cd0-41f8-9d3a-8d36b7a43243.png"]
 
     func getResponseProductInfoNoDescription() -> ProductResponse {
         return ProductResponse(id: self.id,
@@ -28,7 +33,7 @@ struct Product {
                                description: nil,
                                last_review: nil,
                                qt: nil,
-                               image: images[0])
+                               image: smallImages.randomElement())
     }
 
     func getResponseProductInfo(qt: Int) -> ProductResponse {
